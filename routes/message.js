499ba14de6a06d78c0c980b875e2a36b035e7e45
Mohd Belal Naim <contactbelalnaim@gamil.com>
2024-05-13 16:57:33 +0530

    Added message routes and socket

--- routes/message.js
+++ routes/message.js
@@ -1,38 +1,59 @@
-const router = require("express").Router()
-const verifyToken = require("../middlewares/verifyToken")
-const Message = require("../model/messages")
+const router = require("express").Router();
+const verifyToken = require("../middlewares/verifyToken");
+const Message = require("../model/messages");
 
 router.post("/send/:id", verifyToken, (req, res) => {
-    let { id } = req.params
-    const userId = req.user
-    const { content } = req.body
-    const message = new Message({
-        from: userId,
-        to: id,
-        content: content,
-        date: new Date()
-    })
-    message.save()
-        .then(() => res.json({ success: "Message sent successfully" })) 
-        .catch(error => {
-            res.json({ error: "Something went wrong" });
-        });
+  let { id } = req.params;
+  const userId = req.user;
+  const { content } = req.body;
+  const message = new Message({
+    from: userId,
+    to: id,
+    content: content,
+    date: new Date(),
+  });
+  message
+    .save()
+    .then(() => res.json({ success: "Message sent successfully" }))
+    .catch((error) => {
+      res.json({ error: "Something went wrong", er: error });
+    });
 });
 
 router.post("/my", verifyToken, (req, res) => {
-    const userId = req.user;
-    Message.find({ $or: [{ from: userId }, { to: userId }] })
-        .then((found) => {
-            if (found) {
-                res.json(found);
-            } else {
-                res.json({ error: "No messages found for the user" });
-            }
-        })
-        .catch((err) => {
-            res.json({ error: "Something went wrong!" });
-        });
+  const userId = req.user;
+  Message.find({ $or: [{ from: userId }, { to: userId }] })
+    .then((found) => {
+      if (found) {
+        res.json(found);
+      } else {
+        res.json({ error: "No messages found for the user" });
+      }
+    })
+    .catch((err) => {
+      res.json({ error: "Something went wrong!" });
+    });
 });
 
+router.post("/by-chat/:id", verifyToken, (req, res) => {
+  const { id } = req.params;
+  Message.find({
+    $or: [
+      {
+        from: req.user,
+        to: id,
+      },
+      {
+        from: id,
+        to: req.user,
+      },
+    ],
+  })
+    .then((found) => res.json({ found }))
+    .catch((err) => {
+      console.log(err);
+      res.json({ error: "Can't retrieve chats" });
+    });
+});
 
 module.exports = router;
